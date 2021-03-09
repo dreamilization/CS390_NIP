385d11a70e71c94b2e62a8c32060d321c81a69d4
William Wu <chengfeiwu@berkeley.edu>
2021-03-09 18:25:00 -0500

    Initial Commit for Lab 1.

---
+++ lab1/lab1.py
@@ -0,0 +1,305 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+import random
+import matplotlib.pyplot as plt
+
+random.seed(1618)
+np.random.seed(1618)
+# tf.set_random_seed(1618)   # Uncomment for TF1.
+tf.random.set_seed(1618)
+
+# tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# ALGORITHM = "guesser"
+ALGORITHM = "tf_net"
+# ALGORITHM = "tf_conv"
+
+DATASET = "mnist_d"
+# DATASET = "mnist_f"
+# DATASET = "cifar_10"
+# DATASET = "cifar_100_f"
+# DATASET = "cifar_100_c"
+
+if DATASET == "mnist_d":
+    NUM_CLASSES = 10
+    IH = 28
+    IW = 28
+    IZ = 1
+    IS = 784
+    EP = 10
+elif DATASET == "mnist_f":
+    NUM_CLASSES = 10
+    IH = 28
+    IW = 28
+    IZ = 1
+    IS = 784
+    EP = 25
+elif DATASET == "cifar_10":
+    NUM_CLASSES = 10
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = IH * IW * IZ
+    EP = 20
+elif DATASET == "cifar_100_f":
+    NUM_CLASSES = 100
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = IH * IW * IZ
+    EP = 20
+elif DATASET == "cifar_100_c":
+    NUM_CLASSES = 100
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = IH * IW * IZ
+    EP = 20
+
+
+# =========================<Classifier Functions>================================
+
+def guesserClassifier(xTest):
+    ans = []
+    for entry in xTest:
+        pred = [0] * NUM_CLASSES
+        pred[random.randint(0, 9)] = 1
+        ans.append(pred)
+    return np.array(ans)
+
+
+def buildTFNeuralNet(x, y, eps=25):
+    model = keras.Sequential()
+    model.add(tf.keras.layers.Dense(500, input_shape=(IS,), activation='relu'))
+    model.add(tf.keras.layers.Dense(250, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(125, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(250, activation='relu'))
+    model.add(tf.keras.layers.Dense(500, activation='relu'))
+    model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
+    # model.compile(optimizer='adam', loss='categorical_crossentropy')
+    opt = keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=0.01 / eps)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x, y, batch_size=32, epochs=eps, verbose=1)
+    return model
+
+
+def buildTFConvNet(x, y, eps=10, dropout=True, dropRate=0.2):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
+                                     kernel_initializer='he_uniform', input_shape=(IH, IW, IZ)))
+    model.add(tf.keras.layers.BatchNormalization(axis=-1))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(dropRate))
+
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.BatchNormalization(axis=-1))
+    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.BatchNormalization(axis=-1))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(dropRate))
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dense(NUM_CLASSES, activation='softmax'))
+    opt = keras.optimizers.SGD(lr=0.01, momentum=0.9, decay=0.01 / eps)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x, y, epochs=eps, batch_size=32, verbose=0)
+    return model
+
+
+# =========================<Pipeline Functions>==================================
+
+def getRawData():
+    if DATASET == "mnist_d":
+        mnist = tf.keras.datasets.mnist
+        (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+    elif DATASET == "mnist_f":
+        mnist = tf.keras.datasets.fashion_mnist
+        (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+    elif DATASET == "cifar_10":
+        (xTrain, yTrain), (xTest, yTest) = tf.keras.datasets.cifar10.load_data()
+    elif DATASET == "cifar_100_f":
+        (xTrain, yTrain), (xTest, yTest) = tf.keras.datasets.cifar100.load_data(label_mode="fine")
+    elif DATASET == "cifar_100_c":
+        (xTrain, yTrain), (xTest, yTest) = tf.keras.datasets.cifar100.load_data(label_mode="coarse")
+    else:
+        raise ValueError("Dataset not recognized.")
+    print("Dataset: %s" % DATASET)
+    print("Shape of xTrain dataset: %s." % str(xTrain.shape))
+    print("Shape of yTrain dataset: %s." % str(yTrain.shape))
+    print("Shape of xTest dataset: %s." % str(xTest.shape))
+    print("Shape of yTest dataset: %s." % str(yTest.shape))
+    return ((xTrain, yTrain), (xTest, yTest))
+
+def preprocessData(raw):
+    ((xTrain, yTrain), (xTest, yTest)) = raw
+    if ALGORITHM != "tf_conv":
+        xTrainP = xTrain.reshape((xTrain.shape[0], IS))
+        xTestP = xTest.reshape((xTest.shape[0], IS))
+    else:
+        xTrainP = xTrain.reshape((xTrain.shape[0], IH, IW, IZ))
+        xTestP = xTest.reshape((xTest.shape[0], IH, IW, IZ))
+    yTrainP = to_categorical(yTrain, NUM_CLASSES)
+    yTestP = to_categorical(yTest, NUM_CLASSES)
+    xTrainN = xTrainP.astype('float32')
+    xTestN = xTestP.astype('float32')
+    xTrainN /= 255.0
+    xTestN /= 255.0
+    print("New shape of xTrain dataset: %s." % str(xTrainP.shape))
+    print("New shape of xTest dataset: %s." % str(xTestP.shape))
+    print("New shape of yTrain dataset: %s." % str(yTrainP.shape))
+    print("New shape of yTest dataset: %s." % str(yTestP.shape))
+    return ((xTrainN, yTrainP), (xTestN, yTestP))
+
+
+def trainModel(data):
+    xTrain, yTrain = data
+    if ALGORITHM == "guesser":
+        return None  # Guesser has no model, as it is just guessing.
+    elif ALGORITHM == "tf_net":
+        print("Building and training TF_NN.")
+        return buildTFNeuralNet(xTrain, yTrain)
+    elif ALGORITHM == "tf_conv":
+        print("Building and training TF_CNN.")
+        return buildTFConvNet(xTrain, yTrain, EP)
+    else:
+        raise ValueError("Algorithm not recognized.")
+
+
+def runModel(data, model):
+    if ALGORITHM == "guesser":
+        return guesserClassifier(data)
+    elif ALGORITHM == "tf_net":
+        print("Testing TF_NN.")
+        preds = model.predict(data)
+        for i in range(preds.shape[0]):
+            oneHot = [0] * NUM_CLASSES
+            oneHot[np.argmax(preds[i])] = 1
+            preds[i] = oneHot
+        return preds
+    elif ALGORITHM == "tf_conv":
+        print("Testing TF_CNN.")
+        preds = model.predict(data)
+        for i in range(preds.shape[0]):
+            oneHot = [0] * NUM_CLASSES
+            oneHot[np.argmax(preds[i])] = 1
+            preds[i] = oneHot
+        return preds
+    else:
+        raise ValueError("Algorithm not recognized.")
+
+
+def evalResults(data, preds):
+    xTest, yTest = data
+    acc = 0
+    for i in range(preds.shape[0]):
+        if np.array_equal(preds[i], yTest[i]):   acc = acc + 1
+    accuracy = acc / preds.shape[0]
+    print("Classifier algorithm: %s" % ALGORITHM)
+    print("Classifier accuracy: %f%%" % (accuracy * 100))
+    print()
+    return accuracy * 100
+
+
+# =========================<Main>================================================
+
+def main():
+    global DATASET
+    global NUM_CLASSES
+    global IH
+    global IW
+    global IZ
+    global IS
+    global EP
+
+    result = []
+    seq = ['mnist_d', 'mnist_f', 'cifar_10', 'cifar_100_f', 'cifar_100_c']
+
+    DATASET = "mnist_d"
+    NUM_CLASSES = 10
+    IH = 28
+    IW = 28
+    IZ = 1
+    IS = 784
+    EP = 10
+    raw = getRawData()
+    data = preprocessData(raw)
+    model = trainModel(data[0])
+    preds = runModel(data[1][0], model)
+    result.append(evalResults(data[1], preds))
+
+    DATASET = "mnist_f"
+    NUM_CLASSES = 10
+    IH = 28
+    IW = 28
+    IZ = 1
+    IS = 784
+    EP = 25
+    raw = getRawData()
+    data = preprocessData(raw)
+    model = trainModel(data[0])
+    preds = runModel(data[1][0], model)
+    result.append(evalResults(data[1], preds))
+
+    DATASET = "cifar_10"
+    NUM_CLASSES = 10
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = IH * IW * IZ
+    EP = 20
+    raw = getRawData()
+    data = preprocessData(raw)
+    model = trainModel(data[0])
+    preds = runModel(data[1][0], model)
+    result.append(evalResults(data[1], preds))
+
+    DATASET = "cifar_100_f"
+    NUM_CLASSES = 100
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = IH * IW * IZ
+    EP = 20
+    raw = getRawData()
+    data = preprocessData(raw)
+    model = trainModel(data[0])
+    preds = runModel(data[1][0], model)
+    result.append(evalResults(data[1], preds))
+
+    DATASET = "cifar_100_c"
+    NUM_CLASSES = 100
+    IH = 32
+    IW = 32
+    IZ = 3
+    IS = IH * IW * IZ
+    EP = 20
+    raw = getRawData()
+    data = preprocessData(raw)
+    model = trainModel(data[0])
+    preds = runModel(data[1][0], model)
+    result.append(evalResults(data[1], preds))
+
+    plt.plot(seq, result, label='Accuracy')
+    plt.ylabel("Accuracy")
+    plt.legend()
+    if ALGORITHM == "tf_conv":
+        plt.title("CNN Accuracy Plot")
+        plt.savefig('CNN_Accuracy_Plot.pdf')
+    elif ALGORITHM == "tf_net":
+        plt.title("ANN Accuracy Plot")
+        plt.savefig('ANN_Accuracy_Plot.pdf')
+    else:
+        raise ValueError("Algorithm not recognized.")
+
+
+
+if __name__ == '__main__':
+    main()
